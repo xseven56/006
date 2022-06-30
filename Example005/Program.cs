b23042992bd4787c96814a191c609688779f2abb
xseven56 <xseven56@gmail.com>
2022-06-30 23:49:41 +0500

    add Example 005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же маша");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
